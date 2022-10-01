d363f06620e7730fab04334c73bc932fd13b328a
WendelFl <wendelfl.dev@gmail.com>
2022-09-30 21:23:44 -0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+Rails.application.routes.draw do
+  # GET /about
+  get "about", to: "about#index"
+  
+  root to: "main#index"
+  #get "/", to: "main#index"  
+end
